2ef492508d2ae5dc7efa9ad45e2744cc2b91e5b3
AMEENA NAZRIN <nargeesameena@gmail.com>
2025-06-04 20:04:24 +0530

    footer set

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
-
   return (
-    <>
-      <NavBar></NavBar>
-    </>
-  )
+    <div className="app-container">
+      <NavBar />
+      <main className="main-content">
+        {/* Your page content goes here */}
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App
