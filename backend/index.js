8bba9501475ae40f29a70a92cef9d3eb4a34cab3
Anna27-m <annamarymartin652@gmail.com>
2025-06-13 01:32:24 +0530

    db updated

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,13 @@
+var express = require('express')
+var app = express();
+require("./db");
+var profiles=require("./model/people");
+app.use(express.json());
 
-console.log("Backend folder initialized");
+var port=3000;
+
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+})
