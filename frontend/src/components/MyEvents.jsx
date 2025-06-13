99225fd30ea6e395a2bb515ec16a5d37a8ab89c4
hashir-0 <muhammed777hashir@gmail.com>
2025-06-13 11:56:04 +0400

    Create MyEvents.jsx

---
+++ frontend/src/components/MyEvents.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+function MyEvents() {
+  return (
+    <div>
+      <Button variant='contained'>
+            <Link to={"/v"}>volunteers list</Link>
+      </Button>
+    </div>
+    )
+}
+
+export default MyEvents
+
