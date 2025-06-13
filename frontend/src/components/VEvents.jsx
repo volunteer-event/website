a56df1184488396b03b4eb99819ce1284d4ab3e3
hashir-0 <muhammed777hashir@gmail.com>
2025-06-13 11:56:53 +0400

    Create VEvents.jsx

---
+++ frontend/src/components/VEvents.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+function VEvents() {
+  return (
+    <div>
+      <Button variant='contained'>
+            <Link to={"/vl"}>volunteers list</Link>
+      </Button>
+    </div>
+    )
+}
+
+export default VEvents
+
+
